c00bc521001241cf3f4555ba93f2736d5f3789bd
Pavel Radkevich <pradkevich@elinext.com>
2019-04-30 16:04:36 +0300

    initial commit

---
+++ src/main/java/by/pr/feignthymeleaf/FeignThymeleafApplication.java
@@ -0,0 +1,13 @@
+package by.pr.feignthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/pr/feignthymeleaf/FeignThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package by.pr.feignthymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeignThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
